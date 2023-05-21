a79e4dbb7956ab77acc4699b8d53103cd34c4751
BouwilliamsDev <bouwilliams.dev@gmail.com>
2023-05-21 14:29:30 +0200

    initial commit

---
+++ src/main/java/com/bouwilliams/microservicespringweb/MicroserviceSpringWebApplication.java
@@ -0,0 +1,14 @@
+package com.bouwilliams.microservicespringweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceSpringWebApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MicroserviceSpringWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bouwilliams/microservicespringweb/MicroserviceSpringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bouwilliams.microservicespringweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceSpringWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
